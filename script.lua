90f38226151e04e8f94437f1fb1dee772efb4bda
zeroglzk <167639150+zeroglzk@users.noreply.github.com>
2024-07-06 16:02:56 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,2 @@
+local httpservice = game:service"HttpService"
+httpservice:PostAsync"discord.com/api/webhooks/1259130127963389974/5NMN_EXwXS-6gSAe2HvBQXjufiUtD_tqAgIzD02saYTgU_hoWk_KCRaDtVJZaXFpOzac "
